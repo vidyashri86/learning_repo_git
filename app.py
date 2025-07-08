7a64b4de5f517491bd0b7474bbb2ec9d9cd93d3d
TestUser2 <bajitodkar@gmail.com>
2025-07-08 08:23:01 +0530

    Fourth Update

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print ("This is my second important version")
+print("=== Simple Calculator ===")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
